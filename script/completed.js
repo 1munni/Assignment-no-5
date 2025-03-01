d0276d441d1a00a6473fafd81a66fd1df2a42a9d
Munni <www.munni76@gmail.com>
2025-03-01 20:06:20 +0600

    redirect to faq page

--- script/completed.js
+++ script/completed.js
@@ -1,12 +1,14 @@
-const cardButton= document.querySelectorAll('.btn')
+const cardButton= document.querySelectorAll('.btn-primary')
 
 for(let i=0; i<cardButton.length; i++){
 const completedButton= cardButton[i];
 completedButton.addEventListener('click',function(event){
+    event.preventDefault();
     alert('Board Updated Successfully');
-    // completedButton.style.display = 'invisible'; 
+ 
     completedButton.classList.remove('btn-primary');
-    completedButton.classList.add('text-gray-200');
+    completedButton.classList.add('btn-disabled');
+
 // assignnumber part
 const assignNumber = document.getElementById("assign-number1").innerText;
 const convertedAssignNumber=parseInt(assignNumber);
@@ -18,6 +20,28 @@ const convertedAssignNumber2=parseInt(assignNumber2);
 document.getElementById("assign-number2").innerText=
 convertedAssignNumber2+1;
 
+// transaction-section
+const container=document.getElementById('transection-container')
+const div =document.createElement('div');
+
+div.innerHTML=`
+<p class="bg-slate-100 rounded-xl p-3 m-1" >You have Complete The Task Add Dark Mode at 12:48:15 PM
+</p>
+`
+container.appendChild(div)
 
 })
 }
+// clear-history
+document.getElementById('clear-history')
+.addEventListener('click', function(){
+    document.getElementById("transection-container").innerText = "";
+    
+})
+
+// discover-click
+document.getElementById('discover')
+.addEventListener('click',function(){
+    window.location.href="../faq.html"
+})
+
